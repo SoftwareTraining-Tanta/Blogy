d7a2056aac8cb87fed4ae7d78ff9206717adbfa5
ahmedyounis253 <amy2532001@gmail.com>
2022-05-14 20:26:22 +0200

    fixing forignkeys

--- BlogyBackend/Models/Comment.cs
+++ BlogyBackend/Models/Comment.cs
@@ -29,8 +29,7 @@ namespace BlogyBackend.Models
         [ForeignKey(nameof(Username))]
         [InverseProperty(nameof(User.Comments))]
         public virtual User UsernameNavigation { get; set; } = null!;
-        public int adminId {get;set;}
-        public Admin admin {get;set;}
-        
+        public string? AdminUsername {get; set;}
+        public Admin? admin {get;set;}
     }
 }
--- BlogyBackend/Models/Plan.cs
+++ BlogyBackend/Models/Plan.cs
@@ -23,8 +23,8 @@ namespace BlogyBackend.Models
         [ForeignKey(nameof(Username))]
         [InverseProperty(nameof(User.Plans))]
         public virtual User UsernameNavigation { get; set; } = null!;
-        public int adminId {get;set;}
-        public Admin admin {get;set;}
+        public string? AdminUsername {get; set;}
+        public Admin? admin {get;set;}
         public void Add(Plan plan)
         {
             using (blogyContext db = new())
